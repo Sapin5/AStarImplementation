4bf9a2d99691f347da1a205a1daf8547e4176354
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2026-04-21 13:33:32 -0500

    its done
    Its done, A* is done.
    Fixed memory leak (it hit 31gb of ram usage at one point)
    
    to do: Make the player visually traverse using path found by A*

--- AStarImplementation.cpp
+++ AStarImplementation.cpp
@@ -46,10 +46,9 @@ enum CellType {
 
 /*
 * To Do
-* Get rid of all magic numbers <- I dont want to do this. Code is art and should be left to interpertation
-* Implement game state change from wall placement to map traversal
-* Astar algorithm
-* Change the OS check to use an Enum for easier readibility (theres way too many 0s
+* Get rid of all magic numbers <- I dont want to do this. Code is art and should be left to interpertation <- done
+* Implement game state change from wall placement to map traversal  <- done
+* Astar algorithm  <- almost done theres a bug somewhere I cant find
 */
 
 struct coordinate {
@@ -79,14 +78,26 @@ struct coordinate {
         (adjustForVisual) ? this->y = std::clamp(this->y + stepSize, 0, trueColLength) : 
                             this->x = std::clamp(this->x + stepSize + spacing, consoleStepSize, columns * consoleStepSize);
     }
+
+
+    bool operator==(const coordinate& other) const {
+        return (x == other.x && y == other.y);
+    }
+
+    bool operator!=(const coordinate& other) const {
+        return !(*this == other);
+    }
 };
 
 
 struct Cell {
     int id{ 0 };
-    CellType type{ EMPTY }; // string -> enum. Changed because this will likely work better for reading wall types 
+    CellType type{ EMPTY };
     coordinate loc{};
-    int weight{ 0 }; // Not needed due to simple implementation?
+    int weight{ 1 };
+    int g{ 10000 };
+    int f{ 0 };
+    coordinate parent{ -1, -1 };
 };
 
 
@@ -254,7 +265,7 @@ bool checkPosition(coordinate& p, coordinate& g) {
 * heuristic += cross*0.001
 */
 int heuristic(coordinate& p, coordinate& g) {
-    int D = 1; // Cost of moving, Cost is determined  
+    int D = 1; // Cost of moving, Cost is determined. This would alos work as the cell weight. Will not be pulling though
     int dx = abs(p.x - g.x);
     int dy = abs(p.y - g.y);
     return D * (dx + dy);
@@ -271,35 +282,88 @@ std::vector<coordinate> neighbors(coordinate& node, std::array<std::array<Cell, 
     return results;
 }
 
-void aStarSearch(coordinate& p, coordinate& g, std::array<std::array<Cell, rows>, columns>& grid) {
-    // OPEN = priority queue containing START
+// Theres a memory leak somewhere in here. I hit 31GB of ram usage
+std::vector<coordinate> aStarSearch(coordinate& p, coordinate& g, std::array<std::array<Cell, rows>, columns>& grid) {
 
-    std::list<Cell> open{grid[p.x][p.y]};
+    // OPEN = priority queue containing START
+    std::vector<coordinate> open{p};
+    grid[p.x][p.y].g = 0;
+    grid[p.x][p.y].f = heuristic(p, g);
+    grid[p.x][p.y].parent = { -1, -1 };
 
     // CLOSED = empty set
-    std::list<Cell> closed{};
-
+    std::vector<coordinate> closed{};
 
     // while lowest rank in OPEN is not the GOAL:
     while (!open.empty()) {
-        auto current = 0;
-        // current = remove lowest rank item from OPEN
-        // add current to CLOSED
 
-        /*
-        for neighbors of current:
-        cost = g(current) + movementcost(current, neighbor)
-        if neighbor in OPEN and cost less than g(neighbor):
-        remove neighbor from OPEN, because new path is better
-        if neighbor in CLOSED and cost less than g(neighbor): ⁽²⁾
-        remove neighbor from CLOSED
-        if neighbor not in OPEN and neighbor not in CLOSED:
-        set g(neighbor) to cost
-        add neighbor to OPEN
-        set priority queue rank to g(neighbor) + h(neighbor)
-        set neighbor's parent to current
-        */
+        // current = remove lowest rank item from OPEN
+        // This took abt 3 hours to figure out
+        auto comparator = [&grid, &p, &g](const coordinate& a, const coordinate& b) {
+            return grid[a.x][a.y].f < grid[b.x][b.y].f;
+        };
+        std::vector<coordinate>::iterator current_ = std::min_element(open.begin(), open.end(), comparator);
+        coordinate current = *current_;
+        open.erase(current_);
+
+        // add current to CLOSED
+        closed.push_back(current);
+        if (current == g) break;
+        // for neighbors of current:
+        for (coordinate& neighbor : neighbors(current, grid)) {
+            
+            // Ignore walls
+            // Maybe move this to neighbor check
+            if (grid[neighbor.x][neighbor.y].type == WALL) continue;
+
+            // cost = g(current) + movementcost(current, neighbor)
+            float cost = grid[current.x][current.y].g + grid[neighbor.x][neighbor.y].weight;
+           
+            // if neighbor in OPEN and cost less than g(neighbor):
+            // remove neighbor from OPEN, because new path is better
+            float neighborG = grid[neighbor.x][neighbor.y].g;
+            auto inOpen = std::find(open.begin(), open.end(), neighbor);
+            if (inOpen != open.end() && cost < neighborG) {
+                open.erase(inOpen);
+                inOpen = open.end(); // Clear the search query. Needed for running another find later with no issues
+            }
+
+            // if neighbor in CLOSED and cost less than g(neighbor): ⁽²⁾
+            // remove neighbor from CLOSED
+            auto inClosed = std::find(closed.begin(), closed.end(), neighbor);
+            if (inClosed != closed.end() && cost < neighborG) {
+                closed.erase(inClosed);
+                inClosed = closed.end();
+            }
+
+            // if neighbor not in OPEN and neighbor not in CLOSED:
+            // set g(neighbor) to cost
+            // add neighbor to OPEN
+            // set priority queue rank to g(neighbor) + h(neighbor)
+            // set neighbor's parent to current
+            if (inOpen == open.end() && inClosed == closed.end()) {
+                grid[neighbor.x][neighbor.y].g = cost;
+                grid[neighbor.x][neighbor.y].f = cost + heuristic(neighbor, g);
+                grid[neighbor.x][neighbor.y].parent = current;
+                open.push_back(neighbor);
+            }
+        }
+        
     }
+
+    // Reconstruct reverse path from goal to start
+    std::vector<coordinate> path;
+    coordinate curr = g;
+    // start node's parent is {-1, -1}
+    while (curr.x != -1 && curr.y != -1) {
+        path.push_back(curr);
+        if (curr == p) break; // break once we reach start
+        curr = grid[curr.x][curr.y].parent;
+    }
+    // Reverse so it goes start -> end
+    std::reverse(path.begin(), path.end());
+    
+    return path;
 }
 
 
@@ -314,7 +378,7 @@ int main()
         coordinate goal{ rand() % rows, rand() % columns };
         coordinate cursorLoc{ consoleStepSize, 0 };
         coordinate arrayTraverser{ 0, 0 };
-
+        std::vector<coordinate> path{};
 
         int tracker = 0;
         for (int i = 0; i < grid.size(); i++) {
@@ -324,7 +388,7 @@ int main()
                 grid[i][j].loc.x = i;
                 grid[i][j].loc.y = j;
 
-                if (playerPos.x == grid[i][j].loc.x && playerPos.x == grid[i][j].loc.y) {
+                if (playerPos.x == grid[i][j].loc.x && playerPos.y == grid[i][j].loc.y) {
                     grid[i][j].type = PLAYER;
                 }
                 else if(goal.x == grid[i][j].loc.x && goal.y == grid[i][j].loc.y){
@@ -343,10 +407,22 @@ int main()
             printGrid(grid);
             gotoxy(cursorLoc.x, cursorLoc.y);
             moveCursor(grid, cursorLoc, arrayTraverser, goal, playerPos);
-            aStarSearch(playerPos, goal, grid);
+           if (placeWalls) { 
+                path = aStarSearch(playerPos, goal, grid); 
+                play = false;
+            }
             moveAround(playerPos, grid);
         }
-        
+
+        if (path.size() == 0) {
+            std::cout << "No Path Found";
+        }
+        else {
+            std::cout << "Path Found" << std::endl;
+            for (auto& test : path) {
+                std::cout << "{ " << test.x << ", " << test.y << " } ";
+            }
+        }
     }
     else {
         std::cout << "This program currently only runs on windows, sorry";
